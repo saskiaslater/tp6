16b2a1db23fb1a4e9579c62e66c6bd4dbb7ca7a4
Saskia <saskia.slater@wsu.edu>
2021-10-06 23:59:14 +0000

    final edits to java, css and html

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
 
-window.onload = function() {myFunction()};
-
-function myFunction() {
-  document.getElementById("foodbkgd").classList.add("title");
+window.onload = function() {
   
-}
+  document.querySelector("#foodbkgd").classList.add("title");
+
+  
+  document.querySelector("#ingredients").onclick = function() {
+    document.querySelector("#ingredients ul").classList.toggle("showMe");
+  }
+    
+  document.querySelector("#equipment").onclick = function() {
+    document.querySelector("#equipment ul").classList.toggle("showMe");
+  }
+    
+  document.querySelector("#directions").onclick = function() {
+    document.querySelector("#directions ol").classList.toggle("showMe");
+  }
+ 
+    document.querySelector("#foodbkgd").onclick = function() {
+    this.classList.toggle("title")
+  }
+    
+  document.querySelector(".copyright").innerHTML += "<p><strong>This recipe was taken for the purpose of a school project for DTC 477 at Washington State University.<strong></p>";
+  
+} // end of window.onload
